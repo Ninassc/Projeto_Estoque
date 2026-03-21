68560c70b174f168b0775e8827b3dedb18765264
Ninassc <ninasepulvedacarvalho@gmail.com>
2026-03-21 16:16:50 -0300

    correção de erros (main na raiz)

--- funcoes/lerbanco.py
+++ funcoes/lerbanco.py
@@ -1,18 +1,18 @@
 import json
 
-path = "database.json"
+path = "database/database.json"
 
 def ler_banco():
     with open(path, "r", encoding="utf-8") as arquivo:
         dados = json.load(arquivo)
         return dados
 
-# def salvar():
-#     from main_func import login_funcionario, estoque, movimentacoes
-#     dados_atualizados = {
-#         "login_funcionario": login_funcionario,
-#         "estoque": estoque,
-#         "movimentacoes": movimentacoes
-#     }
-#     with open(path, "w", encoding="utf-8") as arquivo:
-#         json.dump(dados_atualizados, arquivo, indent=4, ensure_ascii=False)
+def salvar():
+    from funcoes.main_func import login_funcionario, estoque, movimentacoes
+    dados_atualizados = {
+        "login_funcionario": login_funcionario,
+        "estoque": estoque,
+        "movimentacoes": movimentacoes
+    }
+    with open(path, "w", encoding="utf-8") as arquivo:
+        json.dump(dados_atualizados, arquivo, indent=4, ensure_ascii=False)
